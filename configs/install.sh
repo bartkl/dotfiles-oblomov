ded824304d42b905987435c04091cb5cc15d3752
Bart Kleijngeld <bartkl@gmail.com>
2019-05-31 10:31:31 +0200

    added hosts and sshd_config

--- configs/install.sh
+++ configs/install.sh
@@ -2,12 +2,14 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # system
-ln -s $SCRIPTDIR/system/asound.conf            /etc/asound.conf
-ln -s $SCRIPTDIR/system/fstab                  /etc/fstab
-ln -s $SCRIPTDIR/transmission/settings.json    /etc/transmission-daemon/settings.json
-ln -s $SCRIPTDIR/mpd/mpdscribble.conf          /etc/mpdscribble.conf
-ln -s $SCRIPTDIR/mpd/mpd.conf                  /etc/mpd.conf
+ln -s $SCRIPTDIR/system/asound.conf            /etc/
+ln -s $SCRIPTDIR/system/fstab                  /etc/
+ln -s $SCRIPTDIR/system/hosts                  /etc/
+ln -s $SCRIPTDIR/mpd/mpdscribble.conf          /etc/
+ln -s $SCRIPTDIR/mpd/mpd.conf                  /etc/
+ln -s $SCRIPTDIR/transmission/settings.json    /etc/transmission-daemon/
 ln -s $SCRIPTDIR/mpd/update-mpd.service        /etc/systemd/system/
+ln -s $SCRIPTDIR/system/sshd_config            /etc/ssh/
 
 # user
 runuser -l bart "-c ln -s $SCRIPTDIR/zsh/.zshrc        ~/.zshrc"
