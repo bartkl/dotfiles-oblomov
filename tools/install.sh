68c39d15d6143d38312dc907bc105b6afb48f9a6
Bart Kleijngeld <bartkl@gmail.com>
2020-03-30 23:11:14 +0200

    formatting

--- tools/install.sh
+++ tools/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ln -s $SCRIPTDIR/backup             /usr/local/bin/  # change owner to user!
-ln -s $SCRIPTDIR/beets2mpd          /usr/local/bin/
-ln -s $SCRIPTDIR/update-mpd         /usr/local/bin/
-ln -s $SCRIPTDIR/videoplay          /usr/local/bin/
-ln -s $SCRIPTDIR/tv	            /usr/local/bin/
+ln -s $SCRIPTDIR/backup                              /usr/local/bin/  # change owner to user!
+ln -s $SCRIPTDIR/beets2mpd                           /usr/local/bin/
+ln -s $SCRIPTDIR/update-mpd                          /usr/local/bin/
+ln -s $SCRIPTDIR/videoplay                           /usr/local/bin/
+ln -s $SCRIPTDIR/tv                                  /usr/local/bin/
+ln -s $SCRIPTDIR/transmission-completion-handler     /usr/local/bin/
